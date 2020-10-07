156896da43052429a430fdad405bd53045640220
Hugo Solar <hugo.solar@gmail.com>
2020-10-07 14:11:36 -0300

    new wide template

---
+++ template-wide-no-sidebar.php
@@ -0,0 +1,32 @@
+<?php
+	/** Template name: Wide & no sidebar */
+
+	get_header();
+	the_post();
+?>
+<section class="main-content">
+	<header class="single-header">
+		<div class="container">
+			<div class="columns is-centered">
+				<div class="column is-10">
+					<?php get_template_part( 'inc/partials/entry/page', 'header' ); ?>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container">
+		<div class="columns is-centered">
+			<div class="column is-10">
+				<section class="entry-page-content">
+					<div class="text-format body-big">
+						<?php the_content(); ?>
+					</div>
+					<footer class="entry-footer">
+						<?php get_template_part( 'inc/partials/entry/page', 'footer' ); ?>
+					</footer>
+				</section>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer(); ?>
